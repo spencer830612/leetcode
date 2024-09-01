01359a71fe4bb50a5b0f25c5fad4365544f9114d
spencer830612 <asd59632+github@gmail.com>
2024-09-01 17:38:59 +0800

    新增題目 155； Min Stack

---
+++ src/main/kotlin/code/S155MinStack.kt
@@ -0,0 +1,37 @@
+package org.leetcode.code
+
+import java.util.Stack
+
+class S155MinStack {
+    init {
+
+    }
+    private val stack: Stack<StackAndMin> = Stack()
+
+    fun init (){
+
+    }
+    fun push(`val`: Int) {
+        val previousMin = if(stack.isEmpty) `val` else stack.peek().minToNow
+        val min = min(`val`, previousMin)
+        val stackElement = StackAndMin(`val`, min)
+        stack.push(stackElement)
+    }
+
+    fun pop() {
+        stack.pop()
+    }
+
+    fun top(): Int {
+        return stack.peek().number
+    }
+
+    fun getMin(): Int {
+        return stack.peek().minToNow
+    }
+
+    data class StackAndMin(
+        val number: Int,
+        val minToNow: Int
+    )
+}
